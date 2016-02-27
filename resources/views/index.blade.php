8a775211a8f78d68aa56a51ecdb9794012f3b0eb
Sebastjan Koci <sebastjan.koci@gmail.com>
2016-02-27 19:13:50 +0100

    pohodno drustvo

---
+++ resources/views/index.blade.php
@@ -0,0 +1,53 @@
+@extends("base")
+
+@section("tab_title")
+    Pohodo društvo
+@endsection
+
+@section("page_title")
+
+    Glavna stran
+
+@endsection
+
+@section("content")
+
+    <div class="container">
+
+        <hr class="featurette-divider">
+
+        <!-- First Featurette -->
+        <div class="featurette" id="about">
+            <img class="featurette-image img-circle img-responsive pull-right" src="http://images.8tracks.com/cover/i/000/240/852/500-7536.jpg?rect=0,0,500,500&q=98&fm=jpg&fit=max">
+            <h2 class="featurette-heading">The First Heading
+                <span class="text-muted">Will Blow Your Mind</span>
+            </h2>
+            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <!-- Second Featurette -->
+        <div class="featurette" id="services">
+            <img class="featurette-image img-circle img-responsive pull-left" src="https://s-media-cache-ak0.pinimg.com/564x/85/5b/f8/855bf87fab23038c29be84b5677dd5d9.jpg">
+            <h2 class="featurette-heading">The Second Heading
+                <span class="text-muted">Is Pretty Cool Too.</span>
+            </h2>
+            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <!-- Third Featurette -->
+        <div class="featurette" id="contact">
+            <img class="featurette-image img-circle img-responsive pull-right" src="http://cdnstatic.visualizeus.com/thumbs/f8/b5/landscape,mountain,photography,water-f8b5b505818b403476a1317f707e58aa_h.jpg">
+            <h2 class="featurette-heading">The Third Heading
+                <span class="text-muted">Will Seal the Deal.</span>
+            </h2>
+            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+
+        <hr class="featurette-divider">
+    </div>
+
+@endsection
---
+++ resources/views/smarna.blade.php
@@ -0,0 +1,25 @@
+@extends("base")
+
+@section("tab_title")
+    Šmarna gora
+@endsection
+
+@section("page_title")
+    Šmarna gora
+@endsection
+
+@section("content")
+
+    <div class="container">
+        <div class="featurette" id="about">
+            <img class="featurette-image img-circle img-responsive pull-right" src="http://s3.favim.com/orig/40/forest-green-greenery-nature-stairs-Favim.com-332194.jpg">
+            <h2 class="featurette-heading">Pohod na Šmarno goro
+                <span class="text-muted">Do vrha smo prišli vsi :)</span>
+            </h2>
+            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+
+        <hr class="featurette-divider">
+    </div>
+
+@endsection
